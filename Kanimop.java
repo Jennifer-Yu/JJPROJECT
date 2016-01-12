3e2271e05956b80def2533ec3cd329548290e70f
Jennifer-Yu <jyu6@stuy.edu>
2016-01-12 18:38:23 -0500

    main file created

---
+++ Kanimop.java
@@ -0,0 +1,28 @@
+// MAIN FILE
+
+// INTRO
+public void intro() {
+    String text;
+
+    text = "Welcome to Kanimop!\n";
+
+    text += "\nWould you like to create a character?\n";
+    text += "\t1: Yeah, let's go!";
+    text += "\t2: Nah, maybe later.";
+
+    text += "Your choice: ";
+    System.out.println(s);
+}
+    
+// QUIZ
+public void quiz() {
+    String text;
+    
+
+
+    text += 
+}
+
+public static void main(String[] args) {
+
+}
