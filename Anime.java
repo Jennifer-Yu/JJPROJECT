159209445c1287eaaffac62470db6baf28393cc2
jyang8 <jyang8@stuy.edu>
2016-01-12 14:50:10 -0500

    Update Anime.java

--- Anime.java
+++ Anime.java
@@ -1,120 +1,126 @@
 // ANIME SKELETON IN DATABASE
 
+import java.util.ArrayList;
+
 public class Anime {
 
 
     //INSTANCE VARIABLES
     
-    public String japaneseName;
+    public String name;
 
-    public String englishName;
+    public String alternateName;
 
-    public String[] genres;
+    public ArrayList<String> genres;
 
-    public String[] characters;
+    public ArrayList<String> characters;
 
     public double rating;
 
     public int episodes;
 
-    public String[] reviews;
+    public ArrayList<String> reviews;
 
     public String plot;
 
     //CONSTRUCTORS
 
-    public String japaneseName() {
-	return japaneseName;
-    }
-
-    public String englishName() {
-	return englishName;
-    }
-
-    public String[] genres() {
-	return genres;
-    }
-
-    public String[] characters() {
-	return characters;
-    }
-
-    public double rating() {
-	return rating;
-    }
-
-    public int episodes() {
-	return episodes;
-    }
-
-    public String[] reviews() {
-	return reviews;
-    }
-
-    public String plot() {
-	return plot;
-    }
-
-    //METHODS
-
-    public void japaneseName(String input) {
-	japaneseName = input;
-    }
-
-    public void englishName(String input) {
-	englishName = input;
-    }
-
-    public void genres(String[] input) {
-	genres = input;
-    }
-
-    public void characters(String[] input) {
-	characters = input;
-    }
-
-    public void rating(double input) {
-	rating = input;
-    }
-
-    public void episodes(int input) {
-	episodes = input;
-    }
-
-    public void reviews(String[] input) {
-	reviews = input;
-    }
-
-    public void plot(String input) {
-	plot = input;
-    }
-
-    //ANIMES
-
-    /*
-    public void set() {
+    public Anime() {
 	
-	japaneseName = "Naruto";
-	englishName = "N/A";
+	/*name = "Naruto";
+	alternateName = "N/A";
 	genres = {"Action", "Ninjas", "Shounen"};
 	characters = {"Naruto", "Sakura", "Sasuke"};
 	rating = 5;
 	episodes = 600;
 	reviews = {"Loved it!", "Ew, sucked."};
-	plot = "Ninjas and stuff.";
+	plot = "Ninjas and stuff.";*/
+	
+	name = "Name";
+	alternateName = "N/A";
+	rating = 0;
+	episodes = 0;
+	plot = "Plot";
 
     }
-    */
 
+    //ACCESSORS
 
+    public String getName() {
+	return name;
+    }
+
+    public String getAlternateName() {
+	return alternateName;
+    }
+
+    public ArrayList<String> getGenres() {
+	return genres;
+    }
+
+    public ArrayList<String> getCharacters() {
+	return characters;
+    }
+
+    public double getRating() {
+	return rating;
+    }
+
+    public int getNumEpisodes() {
+	return episodes;
+    }
+
+    public ArrayList<String> getReviews() {
+	return reviews;
+    }
+
+    public String getPlot() {
+	return plot;
+    }
+
+    //MUTATORS
+
+    public void setName(String input) {
+	name = input;
+    }
+
+    public void setAlternateName(String input) {
+	alternateName = input;
+    }
+
+    public void setGenres(ArrayList<String> input) {
+	genres = input;
+    }
+
+    public void setCharacters(ArrayList<String> input) {
+	characters = input;
+    }
+
+    public void setRating(double input) {
+	rating = input;
+    }
+
+    public void setEpisodes(int input) {
+	episodes = input;
+    }
+
+    public void setReviews(ArrayList<String> input) {
+	reviews = input;
+    }
+
+    public void setPlot(String input) {
+	plot = input;
+    }
+
+    //main method for testing
 
     public static void main(String[] args) {
 	Anime Naruto = new Anime();
-	//Naruto.japaneseName("Naruto");
+	//Naruto.name("Naruto");
 
-	Naruto.set();
+	//Naruto.set();
 
-	System.out.println(Naruto.japaneseName());
+	System.out.println(Naruto.name());
 	//System.out.println(
 	//System.out.println(
 	//System.out.println(
